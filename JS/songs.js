c6024acaafa96bb8f1c86d9fd10433c379f53ab8
Konanets <ivankonanezs4@gmail.com>
2022-08-02 07:48:25 +0300

    wt4

---
+++ JS/songs.js
@@ -0,0 +1,12 @@
+function CreatePlayer() {
+  let playListSongs = [
+    {
+      artist: 'Птаха Фред',
+      title: ':(',
+      file: 'https://raw.githubusercontent.com/Konanets/Maine_kampf/master/music/Ptaha_Fred.mp3',
+      duration: '3:21',
+      howl: null
+    },
+  ]
+  return playListSongs;
+}
