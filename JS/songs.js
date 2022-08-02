26215f4383a18e0b8b7d0fddbf4ec5795d1d1450
Konanets <ivankonanezs4@gmail.com>
2022-08-02 07:56:10 +0300

    wt4

--- JS/songs.js
+++ JS/songs.js
@@ -1,12 +1,19 @@
 function CreatePlayer() {
   let playListSongs = [
     {
-      artist: 'Птаха Фред',
-      title: ':(',
-      file: 'https://raw.githubusercontent.com/Konanets/Maine_kampf/master/music/Ptaha_Fred.mp3',
-      duration: '3:21',
+      artist: 'Lucidious',
+      title: 'Moment of Silence',
+      file: 'https://raw.githubusercontent.com/Konanets/Maine_kampf/mini/music/Lucidious-MomentofSilenceftJoshWoods_(yegor.su).mp3',
+      duration: '5:48',
       howl: null
     },
+    {
+      artist: 'NF real music',
+      title: 'Nate',
+      file: 'https://raw.githubusercontent.com/Konanets/Maine_kampf/mini/music/nf-nate(mp3gid.me).mp3',
+      duration: '5:02',
+      howl: null
+    }
   ]
   return playListSongs;
 }
