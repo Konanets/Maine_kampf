29a069576efd7a74615044f3452df8cfbbf3e19f
Konanets <ivankonanezs4@gmail.com>
2022-08-03 04:27:46 +0300

    Мммммм ху%та

--- JS/songs.js
+++ JS/songs.js
@@ -1,5 +1,5 @@
 function CreatePlayer() {
-  let playListSongs = [
+  return [
     {
       artist: 'Lucidious',
       title: 'Moment of Silence',
@@ -15,12 +15,11 @@ function CreatePlayer() {
       howl: null
     },
     {
-      artist: 'NF real music',
-      title: 'Nate',
-      file: 'https://raw.githubusercontent.com/Konanets/Maine_kampf/mini/music/nf-nate(mp3gid.me).mp3',
-      duration: '5:02',
+      artist: 'Infinity Amethyst',
+      title: 'Lena Rain',
+      file: 'https://raw.githubusercontent.com/Konanets/Maine_kampf/mini/music/05%20-%20Infinite%20Amethyst.mp3',
+      duration: '4:31',
       howl: null
     }
-  ]
-  return playListSongs;
+  ];
 }
